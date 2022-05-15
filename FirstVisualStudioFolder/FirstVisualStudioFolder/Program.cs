c80f9f19c5a73891a1d5fa8c9e1985111788e73e
rahmanbello2018@gmail.com <rahmanbello2018@gmail.com>
2022-05-14 22:35:23 -0700

    first_commit

---
+++ FirstVisualStudioFolder/FirstVisualStudioFolder/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+Console.WriteLine("Hello, World!");
+
+List<int> list = new List<int>();
+list.Add(78);
+list.Add(34);
+list.Add(56);
+
+foreach(var ints in list)
+{
+    Console.WriteLine(ints.ToString());
+}
+stringBuilder sb = new 
+
